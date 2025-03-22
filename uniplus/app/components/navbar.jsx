d30c116b83e1d1620203d0c6ec75a5b0ba21f435
Xobi22 <derek.beltran@cetys.edu.mx>
2025-03-22 15:24:59 -0700

    Cambios estructura de Folders, NavBar Component

---
+++ uniplus/app/components/navbar.jsx
@@ -0,0 +1,23 @@
+"use client"
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+export default function NavBar(){
+    const pathName = usePathname();
+    console.log(pathName);
+
+    return(
+        <header role="banner" className="flex p-4 justify-between items-center bg-blue-400">
+            <h1 className="text-2xl font-bold">UNI+</h1>
+            <ul className="flex gap-4">
+                <Link href="/about">
+                    <li className={pathName == "/about" ? "font-bold" : ""}>About</li>
+                </Link>
+                
+                <Link href="/">
+                    <li className={pathName == "/" ? "font-bold" : ""}>Home</li>
+                </Link>
+            </ul>
+        </header>
+    )
+}
