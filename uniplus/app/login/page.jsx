da8a7317a5985352a09cde5f0e1830916eb10e65
EmilioFex20 <emilio.pouget@cetys.edu.mx>
2025-04-23 22:59:54 -0700

    Inicio de sesión funcionando
    Se conectó la función que se tenía en login.js con su componente de react en app/login/page.jsx y ya se puede llenar y enviar el form para comprobar que si exista ese usuario y regresa mensaje de aprobado o de no aprobado. Falta implementar la autenticación para acceder a las partes protegidas.

--- uniplus/app/login/page.jsx
+++ uniplus/app/login/page.jsx
@@ -1,6 +1,28 @@
+"use client";
+
 import Link from "next/link";
 
 export default function LoginPage() {
+  async function onSubmit(event){
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      alert("Se inicio sesión correctamente");
+    } else {
+      alert("Error al registrar inicio de sesión");
+    }
+  }
+
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100 gap-4 ">
       <h1 className="text-black text-3xl">Inicar Sesión</h1>
@@ -10,14 +32,16 @@ export default function LoginPage() {
           Registrate
         </Link>
       </div>
-      <form className="flex flex-col gap-4 mt-4">
+      <form className="flex flex-col gap-4 mt-4" onSubmit={onSubmit}>
         <input
           type="text"
+          name="email"
           placeholder="Correo electrónico"
           className="p-2 border border-gray-300 rounded text-black"
         />
         <input
           type="password"
+          name="password"
           placeholder="Contraseña"
           className="p-2 border border-gray-300 rounded text-black"
         />
