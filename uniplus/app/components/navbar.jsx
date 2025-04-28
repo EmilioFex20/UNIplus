2ed5f851891a19703032f6a52a3f214d01847234
EmilioFex20 <emilio.pouget@cetys.edu.mx>
2025-04-28 16:33:07 -0700

    Logut funcional
    Borra las cookies. Al establecer cookies que vencen en tiempo 0. También ya funciona con el frontend, al hacer click en cerrar sesión te redirige a la página de inicio.

--- uniplus/app/components/navbar.jsx
+++ uniplus/app/components/navbar.jsx
@@ -1,8 +1,21 @@
 "use client"
-import Link from "next/link"
 import { usePathname } from "next/navigation";
 
 export default function NavBar(){
+    async function onClickLogout(){
+        const response = await fetch("/api/auth/logout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (response.ok) {
+            window.location.href = "/";
+        } else {
+            const errorData = await response.json();
+            alert(errorData.message || "Error al cerrar sesión");
+        }
+    }
     const pathName = usePathname();
     console.log(pathName);
 
@@ -10,9 +23,9 @@ export default function NavBar(){
         <header role="banner" className="flex p-4 justify-between items-center bg-blue-400">
             <h1 className="text-2xl font-bold">UNI+</h1>
             <ul className="flex gap-4">
-                <Link href="/about">
+                <button onClick={onClickLogout} className="p-2 bg-blue-500 text-white rounded">
                     Cerrar Sesión
-                </Link>
+                </button>
             </ul>
         </header>
     )
