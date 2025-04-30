4160d982cdaff58b9f4b83655610c7b8af3f9fdd
Xobi22 <derek.beltran@cetys.edu.mx>
2025-04-29 22:42:37 -0700

    Update navbar.jsx

--- uniplus/app/components/navbar.jsx
+++ uniplus/app/components/navbar.jsx
@@ -10,7 +10,7 @@ export default function NavBar(){
 
     return(
         <header>
-          <nav className="w-full bg-white text-white px-6 py-3 flex items-center justify-between">
+          <nav className="w-full bg-white text-white px-6 pt-3 flex items-center justify-between">
             <div className="flex items-center">
               <div className="flex items-center">
                 <Image
