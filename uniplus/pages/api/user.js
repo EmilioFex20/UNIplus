10d824cca1e1ef4bcacfbae6e3f703972a88d448
Xobi22 <derek.beltran@cetys.edu.mx>
2025-05-01 23:46:29 -0700

    Corecciones pequeñas

--- uniplus/pages/api/user.js
+++ uniplus/pages/api/user.js
@@ -16,7 +16,6 @@ export default async function handler(req, res) {
 
     if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
 
-    // Opcional: no mandes la contraseña
     const { password, ...safeUser } = user;
 
     res.status(200).json(safeUser);
