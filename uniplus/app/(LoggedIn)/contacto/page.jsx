848fd6a14d2fe64e9b874e4970e92690ca32c114
Xobi22 <derek.beltran@cetys.edu.mx>
2025-05-02 21:50:23 -0700

    Contact Page Cambios
    se agregaron algunos datos de la BD

--- uniplus/app/(LoggedIn)/contacto/page.jsx
+++ uniplus/app/(LoggedIn)/contacto/page.jsx
@@ -57,7 +57,7 @@ export default function contact() {
   <>
     <div className="flex px-44 ">
       <div className="w-3/5">
-        <p className="text-4xl font-bold pb-2 text-black">{isLoading ? <span className="bg-gray-300 animate-pulse px-4 py-2 rounded w-24 inline-block" /> : `¡Hola ${user.name+user.economiaState}!`}</p>
+        <p className="text-4xl font-bold pb-2 text-black">{isLoading ? <span className="bg-gray-300 animate-pulse px-4 py-2 rounded w-24 inline-block" /> : `¡Hola ${user.name}!`}</p>
         <p className="text-xl pb-4 text-black">Manos a la obra</p>
         <div className="bg-blue-50 border-2 border-[#2f66a5] rounded-lg p-4 mr-15">
           <div className="flex justify-between">
@@ -105,7 +105,7 @@ export default function contact() {
         <hr className="text-white border-b-2 rounded mb-6 "/>
         <div className="text-white text-xl">
           <div className="flex bg-[#1d4e85] rounded px-10 py-2 m-4 justify-between">
-            <p>Cursos Realizados <b className="text-green-400">0/6</b></p><p>Módulos Terminados <b className="text-green-400">0/30</b></p>
+            <p>Cursos Realizados <b className="text-green-400">{isLoading ?"":user.coursesCompleted}/6</b></p><p>Módulos Terminados <b className="text-green-400">{isLoading ?"":user.modulesCompleted}/30</b></p>
           </div>
           <div className="flex bg-[#1d4e85] rounded px-10 py-2 m-4 justify-between">
             <p className="self-center ">
