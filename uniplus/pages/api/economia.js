e9eb8a81f080147aa66069d4ca443e46d9392d9c
Xobi22 <derek.beltran@cetys.edu.mx>
2025-05-03 13:09:17 -0700

    .

--- uniplus/pages/api/economia.js
+++ uniplus/pages/api/economia.js
@@ -16,8 +16,8 @@ export default async function handler(req, res) {
 
     if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
 
-    const { economia } = req.body;
-    user.economia = economia;
+    const { economiaState } = req.body;
+    user.economiaState = economiaState;
     await user.save();
 
     res.status(200).json({ message: "Progreso actualizado" });
